c83ce99d7f8bbb15b7b43f38592bc38ca896ea19
Adam Walker <adamw207@yahoo.com>
2021-04-15 16:57:23 +0000

    Done.

--- spec/artist_spec.rb
+++ spec/artist_spec.rb
@@ -35,9 +35,7 @@ describe "Artist" do
     expect(Artist.find_by(name: "Taylor Swift")).to eq(taytay)
   end
 
-  context "after applying a `db:rollback` command that removes the favorite_food attribute" do
-   it "no longer has a favorite_food column in the 'artists' table" do
-      expect(Artist.column_names).to eq(["id", "name", "genre", "age", "hometown"])
-    end
+  it "can roll back to have no favorite_food attribute for Artist" do    
+    expect(Artist.column_names).to eq(["id", "name", "genre", "age", "hometown"])
   end
 end
