6994f1f63716347b5fa5a564c3698f4a29402a14
Adam Walker <adamw207@yahoo.com>
2021-04-15 16:38:45 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -10,5 +10,10 @@ Bundler.require
 
 
 # put the code to connect to the database here
+ActiveRecord::Base.establish_connection(
+  :adapter => "sqlite3",
+  :database => "db/artists.sqlite"
+)
 
 require_relative "../artist.rb"
+require_relative "../db/migrate/01_create_artists"
